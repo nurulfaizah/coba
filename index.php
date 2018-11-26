57cdb4652844aef680909987c603864c747ff2d9
Nurul Faizah <nurulfaizah1007@gmail.com>
2018-11-26 19:40:03 +0700

    periujbmj

--- index.php
+++ index.php
@@ -265,7 +265,7 @@ switch (ENVIRONMENT)
 		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
 		exit(3); // EXIT_CONFIG
 	}
-
+hjhkhl,
 	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 	// The path to the "views" directory
